640a47baf0923bcfd24e174328267fa2a574bc60
Samuel Sharaf <samuel.sharaf@gmail.com>
2013-07-12 04:49:19 +0000

    modified web.js to read index.html and send the contents to response

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fileHello = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    //read the file
+    var buffer = new Buffer(fs.readFileSync('index.html'));
+    response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
