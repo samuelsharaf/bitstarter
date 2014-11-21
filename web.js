4ccd6c5f972c8d75873bd64a22eab6f72e724574
Samuel Sharaf <samuel.sharaf@salesforce.com>
2014-11-20 16:20:40 -0800

    updated

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+var pg = require('pg');
 
 var app = express.createServer(express.logger());
 app.use(express.static(__dirname + '/images'));
@@ -7,6 +8,18 @@ app.get('/', function(request, response) {
     //read the file
     var buffer = new Buffer(fs.readFileSync('index.html'));
     response.send(buffer.toString('utf-8'));
+});
+
+app.get('/followteam', function(request, response) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+	    client.query('SELECT * FROM test_table', function(err, result) {
+	      done();
+	      if (err)
+	       { console.error(err); response.send("Error " + err); }
+	      else
+	       { response.send(result.rows); }
+	    });
+	  });
 });
 
 var port = process.env.PORT || 8080;
