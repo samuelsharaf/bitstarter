ed4117c8e5c0cb6ca7f956f0218758114420e4de
Samuel Sharaf <samuel.sharaf@salesforce.com>
2014-11-22 15:54:25 -0800

    udpated

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var fs = require('fs');
 var pg = require('pg');
-
 var app = express.createServer(express.logger());
 app.use(express.static(__dirname + '/images'));
+
 app.get('/', function(request, response) {
     //read the file
     var buffer = new Buffer(fs.readFileSync('index.html'));
@@ -13,7 +13,7 @@ app.get('/', function(request, response) {
 app.get('/followteam', function(request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		
-		//console.log('******Received email as: ' + request.query.firstname);
+		console.log('******Received email as: ' + request.query.firstname);
 		
 		var firstName = request.query.firstname;
 		var lastName = request.query.lastname;
@@ -25,27 +25,6 @@ app.get('/followteam', function(request, response) {
                 .fail(function(err) {
                     return next(err);
              	});
-				
-		
-
-		/*client.query('INSERT INTO salesforce.contact (FirstName, LastName) VALUES ($1, $2)', [firstName, lastName], true).then(function() {
-			console.log('******successfully inserted: ' + lastName); 
-            return res.send('ok');
-                
-			})
-                
-			.catch(next);
-		
-	    
-		client.query('SELECT * FROM test_table', function(err, result) {
-	      done();
-	      if (err)
-	       { 
-			   console.error(err); response.send("Error " + err); }
-	      else
-	       { 
-			   response.send('ok'); }
- 	    });*/
 	  });
 });
 
