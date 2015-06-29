8943a35819dde346c2f4d656939ccf5560aa4ee7
Samuel Sharaf <samuel.sharaf@salesforce.com>
2015-06-29 16:44:20 -0700

    dfd

--- web.js
+++ web.js
@@ -11,30 +11,30 @@ app.get('/', function(request, response) {
 });
 
 app.get('/followteam', function(request, response) {
-	
+
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		
+
 		console.log('******Received money as: ' + request.query.money);
-		
+
 		var firstName = request.query.firstname;
 		var lastName = request.query.lastname;
 		var money = request.query.money;
 		var teamid = 'a03j0000001P7Yh';
-		
-		client.query('INSERT into salesforce.follower__c (FirstName__c, LastName__c, Money_Donated__c, Team__c) VALUES ($1, $2, $3, $4)', [firstName, lastName, money, teamid],
+
+		/*client.query('INSERT into salesforce.follower__c (FirstName__c, LastName__c, Money_Donated__c, Team__c) VALUES ($1, $2, $3, $4)', [firstName, lastName, money, teamid],
 		//client.query('INSERT INTO salesforce.contact (FirstName, LastName) VALUES ($1, $2)', [firstName, lastName],
-		function(err, result) 
+		function(err, result)
 		{
-			if (err) 
+			if (err)
 			{
 		    	console.log(err);
 		    } else {
 		    	console.log('************row inserted');
 				response.redirect('/');
 			}
-		 
+
 	  });
-	});
+ });*/
 });
 
 
